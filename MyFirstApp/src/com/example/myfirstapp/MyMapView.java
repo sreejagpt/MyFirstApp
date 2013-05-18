3a42159ae1baaf1a2382d786e5f4cdd0e8e35b26
Sreeja Gupta <sreeja.gupta@uqconnect.edu.au>
2013-05-18 18:00:48 +1000

    Dynamic Plotting works, but slow!
    Considering optimisation. But it is essentially done.

--- MyFirstApp/src/com/example/myfirstapp/MyMapView.java
+++ MyFirstApp/src/com/example/myfirstapp/MyMapView.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -12,6 +14,9 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -26,13 +31,18 @@ import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 
-public class MyMapView extends Activity {
+public class MyMapView extends Activity implements OnItemSelectedListener {
 
 	  private GoogleMap map;
 	  private HashMap<LatLng, Integer> hm = new HashMap<LatLng, Integer>();
 	  private Spinner spinner_wifi_cell, spinner_access_points;
+	  private ArrayAdapter<Object> accessPointsAdapter;
 	  List<String> list_wifi_cell = Arrays.asList("WiFi", "3G/4G", "All");
 	  List<Object> list_access_points = new ArrayList<Object>();
+	  List<Object> prev_list_access_points = new ArrayList<Object>();
+	  ArrayList<MapPoint> coords = new ArrayList<MapPoint>();
+	  private String previousChoice = "";
+	  private String currentChoice = "All";
 	  @SuppressLint("NewApi")
 	@Override
 	  protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +79,7 @@ public class MyMapView extends Activity {
             
 	    	public void onCameraChange(CameraPosition position) {
 	    		
-	    	    
+	    	   
 	    		//rectify zoom level
 	    		if(position.zoom > 18) {
 	    			//we should not be zoomed in more than level 16
@@ -82,28 +92,50 @@ public class MyMapView extends Activity {
 	    		
                 
                 EntryDB_Helper edbh = new EntryDB_Helper(getApplicationContext());
-    		    ArrayList<MapPoint> coords = edbh.getLatLong();
+                
+    		    coords = edbh.getLatLong();
     		    edbh.close();
-                HashMap<String, String> accessPoints = new HashMap<String, String>();
+                Map<String, String> accessPoints = new TreeMap<String, String>();
+                accessPoints.clear();
+                prev_list_access_points = list_access_points;
     		    
-    		    for (int i = coords.size() - 1; i >= 0; i--) {
+    		    for (int i = 0; i < coords.size(); i++) {
     		    	
     		    	LatLng latLng = new LatLng(coords.get(i).latitude, coords.get(i).longitude);
     		    	
-    		    	if (bounds.contains(latLng) && hm.get(latLng) == null) {
+    		    	if (bounds.contains(latLng)) {
     		    		
     		    		accessPoints.put(coords.get(i).SSID, "-"); //to ensure uniqueness
-    		    		list_access_points = Arrays.asList(accessPoints.keySet().toArray());
-    				    spinner_access_points=(Spinner) findViewById(R.id.access_points_spinner);
-    				    ArrayAdapter<Object> accessPointsAdapter = new ArrayAdapter<Object>(getApplicationContext(),
-    							android.R.layout.simple_spinner_item, list_access_points);
-    					  accessPointsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    					  spinner_access_points.setAdapter(accessPointsAdapter);
+    					 
     		    	}
+    		    	
+    		    	accessPoints.put("All", "-");
+    		    	
+    		    	list_access_points = Arrays.asList(accessPoints.keySet().toArray());
+				    
     		    } //updating access points spinner ends here
+
+    		    if (previousChoice.equals("")) {
+    		    	set_spinner2_adapter();
+    		    }
     		    
-    		    plotHeatPoints(bounds);
+    		    if (currentChoice.equals("All") && 
+    		    		(!(prev_list_access_points.equals(list_access_points)))) {
+    		    	map.clear();
+    		    	plotHeatPoints(coords, map.getProjection().getVisibleRegion().latLngBounds);
+    		    	set_spinner2_adapter(); //this is the last thing i added. feel free to remove.
+    		    	return;
+    		    }
+    		    
+    		    if ((!(currentChoice.equals("All"))) && (!(list_access_points.contains(currentChoice)))) {
+    		    	set_spinner2_adapter();
+    		    }
+    		    
+    		    
             }
+
+	    	
+	    	
         });
 
 	  } 
@@ -117,36 +149,42 @@ public class MyMapView extends Activity {
 				android.R.layout.simple_spinner_item, list_wifi_cell);
 		  wifiCellAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		  spinner_wifi_cell.setAdapter(wifiCellAdapter);
-		  
-		  /////////
-		  
-		  
+
 		
 	}
 
+	 private void set_spinner2_adapter() {
+		 
+		 spinner_access_points=(Spinner) findViewById(R.id.access_points_spinner);
+		 accessPointsAdapter = new ArrayAdapter<Object>(this, android.R.layout.simple_spinner_item, list_access_points); 
+			  accessPointsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			  spinner_access_points.setAdapter(accessPointsAdapter);
+			  
+			//set listener for selecting items
+			spinner_access_points.setOnItemSelectedListener(this);
+	 }
 
-	private void plotHeatPoints(LatLngBounds bounds) {
-		//we have an array of lats and longs from our database, we use those
-		    EntryDB_Helper edbh = new EntryDB_Helper(getApplicationContext());
-		    ArrayList<MapPoint> coords = edbh.getLatLong();
-		    edbh.close();
-		    
-		    
-		    
+	private void plotHeatPoints(List<MapPoint> coords1, LatLngBounds bounds) {
+		
+			
+		    //start with empty map
+			//if (!(currentChoice.equals(previousChoice))) {
+				//map.clear();
+			//}
 		    
 			  
-		    for (int i = coords.size() - 1; i >= 0; i--) {
-		    	int strength = coords.get(i).getWiFiStrength();
-		    	LatLng latLng = new LatLng(coords.get(i).latitude, coords.get(i).longitude);
+		    for (int i = coords1.size() - 1; i >= 0; i--) {
+		    	int strength = coords1.get(i).getWiFiStrength();
+		    	LatLng latLng = new LatLng(coords1.get(i).latitude, coords1.get(i).longitude);
 		    	
-		    	if (bounds.contains(latLng) && hm.get(latLng) == null) {
-		    		hm.put(latLng, strength); 
+		    	if (bounds.contains(latLng)) {
+		    		
 		    		
 		    		//plot the heatpoint
 		    		
 		    		//first, get resource 
 		    		List<Integer> resourceInfo = 
-		    				choose_resource(coords.get(i).WifiStrength);
+		    				choose_resource(coords1.get(i).WifiStrength);
 		    		
 		    		
 		    		
@@ -218,6 +256,60 @@ public class MyMapView extends Activity {
 		}
 		
 		return Arrays.asList(R.drawable.hp_deep_blue, 10, 0); //default
+	}
+
+
+	public void onItemSelected(AdapterView<?> parent, View view, int position,
+			long id) {
+		
+		
+		//however, if the item was not clicked , we don't want current choice to update
+		//how to implement that?
+		
+		currentChoice = (String) list_access_points.get((int) parent.getItemIdAtPosition(position));
+		
+		
+		EntryDB_Helper edbh = new EntryDB_Helper(getApplicationContext());
+		ArrayList<MapPoint> coords1 = edbh.getLatLong();
+		edbh.close();
+		if (currentChoice.equals("All")) {
+			if (!(previousChoice.equals(currentChoice))) {
+				map.clear();
+				plotHeatPoints(coords1, map.getProjection().getVisibleRegion().latLngBounds);
+				previousChoice = currentChoice;
+				return;
+			}
+
+			
+			return; //this is important. Function must return at this point.
+		}
+		
+		//we have our selection working! 
+		
+		ArrayList<MapPoint> specialcoords = new ArrayList<MapPoint>();
+		
+		
+		System.out.println("===========");
+		for(int i = 0; i < coords1.size(); i++) {
+			System.out.println("APPMESG SSID: "+coords1.get(i).SSID);
+			if (coords1.get(i).SSID.equals(currentChoice)) {
+				specialcoords.add(coords1.get(i));
+			}
+		}
+		
+		if (!(currentChoice.equals(previousChoice))) {
+			map.clear();
+			plotHeatPoints(specialcoords, map.getProjection().getVisibleRegion().latLngBounds);
+		}
+		System.out.println("APPMESG:: Previous: "+previousChoice+" Current: "+currentChoice);
+		previousChoice = currentChoice;
+		
+	}
+
+
+	public void onNothingSelected(AdapterView<?> arg0) {
+		
+		
 	}
 
 
